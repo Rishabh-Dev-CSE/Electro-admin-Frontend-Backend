e08d4ecb8fe5874e4e7ec63036da85f79fdd4dab
Rishabh-Dev-CSE <rishabhshakya9336@gmail.com>
2026-01-29 08:53:23 -0500

    Remove commented code in PrivateRoute for cleaner implementation

--- docpad-admin-main/src/pages/auth/PrivateRoute.jsx
+++ docpad-admin-main/src/pages/auth/PrivateRoute.jsx
@@ -2,12 +2,8 @@ import { Navigate } from "react-router-dom";
 
 export default function PrivateRoute({ children }) {
   const token = localStorage.getItem("access");
-
-  // token nahi hai → login
   if (!token) {
     return <Navigate to="/auth/login" replace />;
   }
-
-  // token hai → allow
   return children;
 }
