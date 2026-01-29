3c62668f097a4164298c8e5ecb13029c8cb19944
Rishabh-Dev-CSE <rishabhshakya9336@gmail.com>
2026-01-29 00:10:11 -0500

    Refactor comments in PrivateRoute component for clarity

--- docpad-admin-main/src/pages/auth/PrivateRoute.jsx
+++ docpad-admin-main/src/pages/auth/PrivateRoute.jsx
@@ -3,11 +3,11 @@ import { Navigate } from "react-router-dom";
 export default function PrivateRoute({ children }) {
   const token = localStorage.getItem("access");
 
-  // ❌ token nahi hai → login
+  // token nahi hai → login
   if (!token) {
     return <Navigate to="/auth/login" replace />;
   }
 
-  // ✅ token hai → allow
+  // token hai → allow
   return children;
 }
