6ee80b504a07821ec09c942f9f1cb9ff8e764ab2
Rishabh Shakya <163521245+Rishabh-Dev-CSE@users.noreply.github.com>
2026-02-10 07:33:56 -0500

    Refactor MEDIA_ROOT path definition
    Updated MEDIA_ROOT to use os.path.join for consistency.

--- DjangoDB/electricAdmin/settings.py
+++ DjangoDB/electricAdmin/settings.py
@@ -181,9 +181,8 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # =======================
 # MEDIA FILES (Uploads)
 # =======================
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / "media"
-
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 # Simple JWT Tocken
